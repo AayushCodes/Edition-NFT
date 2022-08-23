360da7b8e73a59b68b0e32c2c26eedcc68df53b2
Aayush Gupta <aayushcodes@gmail.com>
2022-08-23 16:57:47 +0530

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import '../styles/style.css' 
+import { ThirdwebProvider } from '@thirdweb-dev/react'
+import { ChainId } from '@thirdweb-dev/sdk'
+import React from 'react';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider desiredChainId={ChainId.Mumbai}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  )
 }
 
 export default MyApp
